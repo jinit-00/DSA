a8eab4041357d8bbcb317d1de0abd7c7e689c72d
jinit-00 <jinitsoneji@gmail.com>
2026-08-10 06:37:22 +0530

    Time: 31 ms (81.44%) | Memory: 9 MB (82.55%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                int remaining = i - j * j;
+
+                // If opponent is in a losing state,
+                // current player wins.
+                if (!dp[remaining]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
