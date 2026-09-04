465162cf1ca7ad87401361daf1f18937a98e9812
jinit-00 <jinitsoneji@gmail.com>
2026-09-04 09:05:30 +0530

    Time: 1 ms (34.01%) | Memory: 30.7 MB (74.15%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int> smallest;
+        int n = nums.size()-1;
+        int val = nums[n];
+        
+        for(int i =n;i>=0;i--)
+        {
+            val = min(val,nums[i]);
+            smallest.push_back(val);
+        }
+
+        reverse(smallest.begin(), smallest.end());
+
+        int greatest = nums[0];
+        for(int i=0;i<=n;i++)
+        {
+
+            greatest = max(greatest , nums[i]);
+            // cout << greatest << " " << smallest[i]
+            if(greatest - smallest[i] <= k)
+            return i;
+        }
+    return -1;
+    }
+};
