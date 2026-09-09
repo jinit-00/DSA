cb00fa3b256c94ec46f3eaafb498036b929f8cd2
jinit-00 <jinitsoneji@gmail.com>
2026-09-09 09:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (57.33%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     long long countCommas(long long n) {
         long long answer = 0;
-        long long val = 1000;
-        while(val <= n)
+        long long lower = 1000;
+        long long commas = 1;
+        while(lower<=n)
         {
-            answer += (n - val + 1);
-            val *= 1000;
+            long long upper = (lower * 1000)-1;
+            if(upper > n )
+                upper = n;
+            answer += (upper - lower+1) * commas;
+            lower *= 1000;
+            commas++;
         }
     return answer;
     }
