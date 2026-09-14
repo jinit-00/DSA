dd3910ca44072d92cd2d59830fb472f963659392
jinit-00 <jinitsoneji@gmail.com>
2026-09-14 12:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.03%) - LeetSync

---
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        //left
+        if(rec2[2] <= rec1[0])
+        return false;
+        if(rec2[1] >= rec1[3])
+        return false;
+        if(rec1[2] <= rec2[0])
+        return false;
+        if(rec2[3] <= rec1[1])
+        return false;
+    return true;
+    }
+};
